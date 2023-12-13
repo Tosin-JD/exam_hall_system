99d066137a6fb3be12d02772b0cb9dff64093dd0
Tosin-JD <oluwatosinjosephdurodola@gmail.com>
2023-12-13 11:45:45 +0100

    make changes to configure for vercel

--- exam_hall_system/urls.py
+++ exam_hall_system/urls.py
@@ -36,4 +36,8 @@ if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL,
                           document_root=settings.MEDIA_ROOT)
     
+
+urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    
 handler404 = 'main.views.page_not_found_view'
